7b0dcf965c5dfc7a00e81ea00adffaea027785db
João Oliveira <jccd.oliveira@gmail.com>
2023-04-04 14:12:34 +0100

    add funct CSV to File

---
+++ ES2023GrupoE/src/utils/Utils.java
@@ -0,0 +1,27 @@
+package utils;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Utils {
+
+	
+    public static void csvToFile(String fileName) {
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            PrintWriter pw = new PrintWriter(fw);
+
+            pw.println("Nome, Idade, Cidade"); // cabeçalho do arquivo
+
+            pw.println("João, 30, São Paulo");
+            pw.println("Maria, 25, Rio de Janeiro");
+            pw.println("Pedro, 40, Belo Horizonte");
+
+            pw.close();
+            fw.close();
+        } catch (IOException e) {
+            System.err.println("Erro ao criar arquivo CSV: " + e.getMessage());
+        }
+    }
+	
+}
