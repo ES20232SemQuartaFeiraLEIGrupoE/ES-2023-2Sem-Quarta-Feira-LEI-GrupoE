8729d69cfc9bef23377f20af4021215fc37029ae
gbotelho7 <botelhg7@gmail.com>
2023-04-12 22:57:15 +0100

    Função que lê de JSON para arraylist feita

--- ES2023GrupoE/src/utils/Main.java
+++ ES2023GrupoE/src/utils/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		Utils.csvToFile("test.csv");
 		System.out.println("main");
+
+		String filename = "/test.json";
+		ArrayList<JSONObject> jsonObjects = JsonReader.readJsonFile(filename);
+		for (JSONObject jsonObject : jsonObjects) {
+			System.out.println(jsonObject.toJSONString());
 	}
 
 }
--- ES2023GrupoE/src/utils/Utils.java
+++ ES2023GrupoE/src/utils/Utils.java
@@ -4,6 +4,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.io.FileReader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class Utils {
 
@@ -24,5 +28,22 @@ public class Utils {
             System.err.println("Erro ao criar arquivo CSV: " + e.getMessage());
         }
     }
+
+    public static ArrayList<JSONObject> jsonToArrayList(String fileName){
+        ArrayList<JSONObject>  list = new JSONParser();
+        JSONParser parser = new JSONParser();
+        try{
+            Object obj = parser.parse(new FileReader(fileName));
+            JSONArray jsonArray = (JSONArray) obj;
+            for (Object obj : jsonArray){
+                JSONObject jsonObject = (JSONObject) o;
+                list.add(jsonObject);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 
 }
