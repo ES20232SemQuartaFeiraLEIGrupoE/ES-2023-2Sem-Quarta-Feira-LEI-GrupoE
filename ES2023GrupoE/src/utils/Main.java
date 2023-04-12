45c8d76a279ff41dea2f8f12637abf683f081848
João Oliveira <jccd.oliveira@gmail.com>
2023-04-12 23:35:22 +0100

    refactor

--- ES2023GrupoE/src/utils/Main.java
+++ ES2023GrupoE/src/utils/Main.java
@@ -3,13 +3,7 @@ package utils;
 public class Main {
 
 	public static void main(String[] args) {
-		Utils.csvToFile("test.csv");
-		System.out.println("main");
 
-		String filename = "test.json";
-		ArrayList<JSONObject> jsonObjects = JsonReader.readJsonFile(filename);
-		for (JSONObject jsonObject : jsonObjects) {
-			System.out.println(jsonObject.toJSONString());
 	}
 
 }
--- ES2023GrupoE/src/utils/Utils.java
+++ ES2023GrupoE/src/utils/Utils.java
@@ -11,23 +11,6 @@ import org.json.simple.parser.JSONParser;
 
 public class Utils {
 
-    public static void csvToFile(String fileName) {
-        try {
-            FileWriter fw = new FileWriter(fileName);
-            PrintWriter pw = new PrintWriter(fw);
-
-            pw.println("Nome, Idade, Cidade"); // cabeçalho do arquivo
-
-            pw.println("João, 30, São Paulo");
-            pw.println("Maria, 25, Rio de Janeiro");
-            pw.println("Pedro, 40, Belo Horizonte");
-
-            pw.close();
-            fw.close();
-        } catch (IOException e) {
-            System.err.println("Erro ao criar arquivo CSV: " + e.getMessage());
-        }
-    }
 
     public static ArrayList<JSONObject> jsonToArrayList(String fileName){
         ArrayList<JSONObject>  list = new JSONParser();
