1853af8c80b2a58408d674d0781fde53a6676b15
João Oliveira <jccd.oliveira@gmail.com>
2023-04-12 23:50:00 +0100

    refactor

--- ES2023GrupoE/src/utils/Utils.java
+++ ES2023GrupoE/src/utils/Utils.java
@@ -1,41 +1,20 @@
 package utils;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.io.FileReader;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class Utils {
-
-    public static void csvToFile(String fileName) {
-        try {
-            FileWriter fw = new FileWriter(fileName);
-            PrintWriter pw = new PrintWriter(fw);
-
-            pw.println("Nome, Idade, Cidade"); // cabeçalho do arquivo
-
-            pw.println("João, 30, São Paulo");
-            pw.println("Maria, 25, Rio de Janeiro");
-            pw.println("Pedro, 40, Belo Horizonte");
-
-            pw.close();
-            fw.close();
-        } catch (IOException e) {
-            System.err.println("Erro ao criar arquivo CSV: " + e.getMessage());
-        }
-    }
-
     public static ArrayList<JSONObject> jsonToArrayList(String fileName){
-        ArrayList<JSONObject>  list = new JSONParser();
+        ArrayList<JSONObject>  list = new ArrayList<>();
         JSONParser parser = new JSONParser();
         try{
             Object obj = parser.parse(new FileReader(fileName));
             JSONArray jsonArray = (JSONArray) obj;
-            for (Object obj : jsonArray){
+            for (Object o : jsonArray){
                 JSONObject jsonObject = (JSONObject) o;
                 list.add(jsonObject);
             }
@@ -44,6 +23,4 @@ public class Utils {
         }
         return list;
     }
-
-
 }
