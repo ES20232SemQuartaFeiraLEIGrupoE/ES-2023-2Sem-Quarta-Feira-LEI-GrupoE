0fb40392159373eecf7f46f967f8f57b801f5116
Tbatista710 <tbatista710@gmail.com>
2023-04-15 21:06:40 +0100

    Create BlockTest
    - criada classe de testes da classe Block

---
+++ ES2023GrupoE/src/model/BlockTest.java
@@ -0,0 +1,17 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlockTest {
+
+    @Test
+    void getHeader() {
+        Block b = new Block();
+    }
+
+    @Test
+    void testToString() {
+    }
+}
