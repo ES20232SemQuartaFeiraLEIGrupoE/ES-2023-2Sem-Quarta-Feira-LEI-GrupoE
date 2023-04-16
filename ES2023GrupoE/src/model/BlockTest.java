a36946c32e370138f5840088def1b23b278f503b
Tbatista710 <tbatista710@gmail.com>
2023-04-16 11:20:44 +0100

    Update BlockTest
    -Added method to test setAttribute

--- ES2023GrupoE/src/model/BlockTest.java
+++ ES2023GrupoE/src/model/BlockTest.java
@@ -8,6 +8,34 @@ class BlockTest {
 
     @Test
     void setAttribute() {
+        Block block = new Block();
+
+        // Set the attributes using the setAttribute() method
+        block.setAttribute(0, "ME");
+        block.setAttribute(1, "Teoria dos Jogos e dos Contratos");
+        block.setAttribute(2, "01789TP01");
+        block.setAttribute(3, "MEA1");
+        block.setAttribute(4, "30");
+        block.setAttribute(5, "Sex");
+        block.setAttribute(6, "13:00:00");
+        block.setAttribute(7, "14:30:00");
+        block.setAttribute(8, "02/12/2022");
+        block.setAttribute(9, "AA2.25");
+        block.setAttribute(10, "34");
+
+
+        assertEquals("ME", block.course);
+        assertEquals("Teoria dos Jogos e dos Contratos", block.curricular_unit);
+        assertEquals("01789TP01", block.shift);
+        assertEquals("MEA1", block.team);
+        assertEquals("30", block.number_of_subscribers);
+        assertEquals("Sex", block.day_of_week);
+        assertEquals("13:00:00", block.hour_begin);
+        assertEquals("14:30:00", block.hour_end);
+        assertEquals("02/12/2022", block.date);
+        assertEquals("AA2.25", block.room);
+        assertEquals("34", block.size_room);
+
     }
 
     @Test
