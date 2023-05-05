dd1b18293fc8147b48caa117ef5d4f2d3fe839f9
gbotelho7 <botelhg7@gmail.com>
2023-05-05 16:50:14 +0100

    changed junit tests folder locations

---
+++ SE-1.0/src/test/java/iscte/se/SE10/JUnitTests/AllTests.java
@@ -0,0 +1,12 @@
+package iscte.se.SE10.JUnitTests;//package iscte.se.SE10.JUnitTests;
+//
+//
+//import org.junit.platform.runner.JUnitPlatform;
+//import org.junit.platform.suite.api.SelectClasses;
+//import org.junit.runner.RunWith;
+//
+//
+//@RunWith(JUnitPlatform.class)
+//@SelectClasses({BlockTest.class, ConvertFilesTest.class, FileReaderWriterTest.class, MainTest.class})
+//public class AllTests {}
+//
